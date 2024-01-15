1bdacf2aca29f8f8df116fd9cd02ac9911e55d9c
elmital <54907162+elmital@users.noreply.github.com>
2024-01-15 18:15:37 +0100

    chore: cleaning

--- src/client/java/be/elmital/fixmcstats/FixMCStatsClient.java
+++ src/client/java/be/elmital/fixmcstats/FixMCStatsClient.java
@@ -1,11 +1,15 @@
 package be.elmital.fixmcstats;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FixMCStatsClient implements ClientModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("fix-mc-stats");
+
 	@Override
 	public void onInitializeClient() {
-		System.out.println("HELLO WORLD");
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		LOGGER.info("Initializing mod");
+		LOGGER.info("Mod initialized!");
 	}
 }
