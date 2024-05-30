d699038c64263c2930d77acd2bfd29d7834deabe
elmital <54907162+elmital@users.noreply.github.com>
2024-05-30 16:41:01 +0200

    feat: fix MC-268093 Breaking a decorated pot with an arrow doesn't affect statistics

---
+++ src/main/java/be/elmital/fixmcstats/mixin/DecoratedPotBlockMixin.java
@@ -0,0 +1,23 @@
+package be.elmital.fixmcstats.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.DecoratedPotBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DecoratedPotBlock.class)
+public class DecoratedPotBlockMixin {
+    // https://bugs.mojang.com/browse/MC-268093
+    @Inject(method = "onProjectileHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;breakBlock(Lnet/minecraft/util/math/BlockPos;ZLnet/minecraft/entity/Entity;)Z"))
+    public void breakBlock(World world, BlockState state, BlockHitResult hit, ProjectileEntity projectile, CallbackInfo ci) {
+        if (projectile.getOwner() instanceof PlayerEntity player)
+            player.incrementStat(Stats.MINED.getOrCreateStat(state.getBlock()));
+    }
+}
