77c8e0378baafdfd4c31587e1d9b9529eb205b22
elmital <54907162+elmital@users.noreply.github.com>
2024-06-01 19:42:35 +0200

    fix (MC-264274) : patch doesn't work anymore

--- src/main/java/be/elmital/fixmcstats/mixin/PlaceableOnWaterItemMixin.java
+++ src/main/java/be/elmital/fixmcstats/mixin/PlaceableOnWaterItemMixin.java
@@ -1,6 +1,8 @@
 package be.elmital.fixmcstats.mixin;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PlaceableOnWaterItem;
 import net.minecraft.stat.Stats;
@@ -13,11 +15,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlaceableOnWaterItem.class)
-public class PlaceableOnWaterItemMixin {
+public class PlaceableOnWaterItemMixin extends BlockItem {
+    public PlaceableOnWaterItemMixin(Block block, Settings settings) {
+        super(block, settings);
+    }
+
     // Fix https://bugs.mojang.com/browse/MC-264274
     @Inject(method = "use", at = @At(value = "RETURN"))
     public void incrementStat(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> result) {
         if (result.getReturnValue().getResult().shouldIncrementStat())
-            user.incrementStat(Stats.USED.getOrCreateStat(result.getReturnValue().getValue().getItem()));
+            user.incrementStat(Stats.USED.getOrCreateStat(this));
     }
 }
