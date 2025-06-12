602b9047a0074f28d7a9dce0e9b6e287ad552d33
elmital <54907162+elmital@users.noreply.github.com>
2025-06-12 17:47:36 +0200

    refactor(Configs): remove enum usages and create a new ConfigEntry class to replace them

--- src/main/java/be/elmital/fixmcstats/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/be/elmital/fixmcstats/mixin/ServerPlayerEntityMixin.java
@@ -1,6 +1,7 @@
 package be.elmital.fixmcstats.mixin;
 
 
+import be.elmital.fixmcstats.Configs;
 import be.elmital.fixmcstats.StatisticUtils;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.block.ScaffoldingBlock;
@@ -26,13 +27,13 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
     // Fix https://bugs.mojang.com/browse/MC-256638
     @ModifyArg(method = "increaseRidingMotionStats", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;increaseStat(Lnet/minecraft/util/Identifier;I)V", ordinal = 3), index = 0)
     private Identifier modifyTargetStat(Identifier identifier) {
-        return getVehicle() instanceof CamelEntity && be.elmital.fixmcstats.Config.instance().USE_CAMEL_CUSTOM_STAT ? StatisticUtils.CAMEL_RIDING_STAT.identifier() : identifier;
+        return getVehicle() instanceof CamelEntity && Configs.CAMEL_STAT.isActive() ? StatisticUtils.CAMEL_RIDING_STAT.identifier() : identifier;
     }
 
     // Fix for https://bugs.mojang.com/browse/MC-148457
     @ModifyArg(method = "increaseTravelMotionStats", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;increaseStat(Lnet/minecraft/util/Identifier;I)V", ordinal = 6), index = 0)
     private Identifier useCrawlStat(Identifier identifier) {
-        if (be.elmital.fixmcstats.Config.instance().USE_CRAWL_CUSTOM_STAT && this.isCrawling())
+        if (Configs.CRAWL_STAT.isActive() && this.isCrawling())
             return StatisticUtils.CRAWL_ONE_CM.identifier();
         return identifier;
     }
