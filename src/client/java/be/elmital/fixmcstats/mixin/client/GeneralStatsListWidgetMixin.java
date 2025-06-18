bc639129726a4e6d4cd8b2de69287e33404292cc
elmital <54907162+elmital@users.noreply.github.com>
2025-06-18 19:56:13 +0200

    refactor: replace the @Redirect usages

--- src/client/java/be/elmital/fixmcstats/mixin/client/GeneralStatsListWidgetMixin.java
+++ src/client/java/be/elmital/fixmcstats/mixin/client/GeneralStatsListWidgetMixin.java
@@ -1,16 +1,14 @@
 package be.elmital.fixmcstats.mixin.client;
 
 import be.elmital.fixmcstats.Configs;
-import com.llamalad7.mixinextras.sugar.Local;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.StatsScreen;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.stat.Stat;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.text.Collator;
 import java.util.Comparator;
@@ -30,13 +28,11 @@ public class GeneralStatsListWidgetMixin {
     }
 
     // Fix https://bugs.mojang.com/browse/MC-178516
-    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/ObjectArrayList;sort(Ljava/util/Comparator;)V", remap = false))
-    private void sort(ObjectArrayList<Stat<net.minecraft.util.Identifier>> instance, Comparator<Stat<net.minecraft.util.Identifier>> comp, @Local(argsOnly = true) MinecraftClient ci) {
+    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/ObjectArrayList;sort(Ljava/util/Comparator;)V", remap = false))
+    private Comparator<Stat<Identifier>> sort(Comparator<Stat<Identifier>> comp) {
         if (Configs.SORTED_STATS_SCREEN.isActive())
-            instance.sort((e1,e2) -> fixCollator.compare(I18n.translate(StatsScreen.getStatTranslationKey(e1)), I18n.translate(StatsScreen.getStatTranslationKey(e2))));
+            return (e1,e2) -> fixCollator.compare(I18n.translate(StatsScreen.getStatTranslationKey(e1)), I18n.translate(StatsScreen.getStatTranslationKey(e2)));
         else
-            instance.sort(Comparator.comparing((statx) -> {
-                return I18n.translate(StatsScreen.getStatTranslationKey(statx), new Object[0]);
-            }));
+            return comp;
     }
 }
