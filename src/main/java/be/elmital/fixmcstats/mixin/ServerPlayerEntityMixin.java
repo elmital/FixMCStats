df7273d9ec4b2b9d346ab2bf1a231b4199aa4d54
elmital <54907162+elmital@users.noreply.github.com>
2025-06-20 18:39:44 +0200

    feat (MC-277294 patch): fix for Distance by mules and donkeys counts towards 'Distance by Horse' statistic

--- src/main/java/be/elmital/fixmcstats/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/be/elmital/fixmcstats/mixin/ServerPlayerEntityMixin.java
@@ -7,6 +7,8 @@ import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.block.ScaffoldingBlock;
 import net.minecraft.entity.passive.CamelEntity;
+import net.minecraft.entity.passive.DonkeyEntity;
+import net.minecraft.entity.passive.MuleEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.packet.c2s.common.SyncedClientOptions;
 import net.minecraft.server.MinecraftServer;
@@ -25,9 +27,17 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
     }
 
     // Fix https://bugs.mojang.com/browse/MC-256638
+    // Fix https://bugs.mojang.com/browse/MC-277294
     @ModifyArg(method = "increaseRidingMotionStats", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;increaseStat(Lnet/minecraft/util/Identifier;I)V", ordinal = 3), index = 0)
     private Identifier modifyTargetStat(Identifier identifier) {
-        return getVehicle() instanceof CamelEntity && Configs.CAMEL_STAT.isActive() ? StatisticUtils.CAMEL_RIDING_STAT.identifier() : identifier;
+        if (getVehicle() instanceof CamelEntity && Configs.CAMEL_STAT.isActive())
+            return StatisticUtils.CAMEL_RIDING_STAT.identifier();
+        else if (getVehicle() instanceof DonkeyEntity && Configs.USE_DONKEY_STATS.isActive())
+            return StatisticUtils.DONKEY_RIDING_STAT.identifier();
+        else if (getVehicle() instanceof MuleEntity && Configs.USE_MULE_STATS.isActive())
+            return StatisticUtils.MULE_RIDING_STAT.identifier();
+        else
+            return identifier;
     }
 
     // Fix for https://bugs.mojang.com/browse/MC-148457
