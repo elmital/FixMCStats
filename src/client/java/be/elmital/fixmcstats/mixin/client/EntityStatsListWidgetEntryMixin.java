52e9fa4e359af3a61d5c4ff9e4a026f916449b43
elmital <54907162+elmital@users.noreply.github.com>
2025-09-29 16:17:47 +0200

    chore : TODO cleaning

--- src/client/java/be/elmital/fixmcstats/mixin/client/EntityStatsListWidgetEntryMixin.java
+++ src/client/java/be/elmital/fixmcstats/mixin/client/EntityStatsListWidgetEntryMixin.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(targets = "net.minecraft.client.gui.screen.StatsScreen$EntityStatsListWidget$Entry")
 @Environment(EnvType.CLIENT)
 public class EntityStatsListWidgetEntryMixin {
-    // TODO MAY BE FIXED CHECK MOJIRA WHEN THE SITE ISN'T BROKEN...
     // Fix https://bugs.mojang.com/browse/MC-80827
     @ModifyVariable(method = "render", at = @At(value = "INVOKE", target ="Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)V", ordinal = 1, shift = At.Shift.AFTER), ordinal = 1, argsOnly = true)
     private int injected(int y) {
