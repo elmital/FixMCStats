13fa47266972e029061957425c1f6c0f1952bf46
elmital <54907162+elmital@users.noreply.github.com>
2025-12-23 17:06:22 +0100

    fix: move the mod init logic in the CommonClass
    (cherry picked from commit 8185e3d1ea1e75d3d90e18dba836e08a120ccd24)

--- common/src/main/java/be/elmital/fixmcstats/CommonClass.java
+++ common/src/main/java/be/elmital/fixmcstats/CommonClass.java
@@ -1,25 +1,45 @@
 package be.elmital.fixmcstats;
 
 import be.elmital.fixmcstats.platform.Services;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.Items;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-// This class is part of the common project meaning it is shared between all supported loaders. Code written here can only
-// import and access the vanilla codebase, libraries used by vanilla, and optionally third party libraries that provide
-// common compatible binaries. This means common code can not directly use loader specific concepts such as Forge events
-// however it will be compatible with all supported mod loaders.
+import java.io.IOException;
+
+
 public class CommonClass {
-    public static final Logger LOGGER = LoggerFactory.getLogger("fix-mc-stats");
-
-    // The loader specific projects are able to import and use any code from the common project. This allows you to
-    // write the majority of your code here and load it from your loader specific projects. This example has some
-    // code that gets invoked by the entry point of the loader specific projects.
     public static void init() {
+        Constants.LOGGER.info("Initializing mod");
+        try {
+            Constants.LOGGER.info("Checking mod config...");
+            Config.initConfig(Constants.LOGGER, Services.PLATFORM.getConfigDir());
+            Constants.LOGGER.info("Mod configured!");
+            // TODO
+            // Constants.LOGGER.info("Checking for custom statistics");
+            // StatisticUtils.registerAllCustomStats(LOGGER);
+            Constants.LOGGER.info("Handling command event for potential server side purpose...");
+            // TODO
+            // BasicCommand.register();
+            Constants.LOGGER.info("Commands registered!");
+            Constants.LOGGER.info("Registering command arguments...");
+            // TODO client side command registration
+            /*
+            Constants.LOGGER.info("Registering commands client side...");
+            ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(BasicCommand.commandNodeBuilder(ModEnvironment.CLIENT, (fabricClientCommandSource, text, aBoolean) -> fabricClientCommandSource.sendFeedback(text))));
+            Constants.LOGGER.info("Commands registered!");
+            */
+            // TODO
+            // BasicCommand.registerArgumentTypes();
+            Constants.LOGGER.info("Command arguments registered!");
+        } catch (SecurityException e) {
+            Constants.LOGGER.trace("Can't load or generate config file! There is a read/write permission issue.", e);
+            throw new RuntimeException(e);
+        } catch (IOException | IllegalArgumentException e) {
+            Constants.LOGGER.trace("Can't load or generate config! If the file exists already delete it and it will be re-created on the next launch with the default values.", e);
+            throw new RuntimeException(e);
+        }
+        Constants.LOGGER.info("Mod initialized!");
 
-        Constants.LOGGER.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
-        Constants.LOGGER.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
+
+
 
         // It is common for all supported loaders to provide a similar feature that can not be used directly in the
         // common code. A popular way to get around this is using Java's built-in service loader feature to create
