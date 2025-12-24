f6b3ef26981a199136cc4a34c02e54f680635541
elmital <54907162+elmital@users.noreply.github.com>
2025-12-24 20:38:38 +0100

    fix(NeoForge): ArgumentType registration
    (cherry picked from commit a5dab713dae8fae30273955429140a0afad1041a)

--- neoforge/src/main/java/be/elmital/fixmcstats/FixMCStats.java
+++ neoforge/src/main/java/be/elmital/fixmcstats/FixMCStats.java
@@ -3,6 +3,8 @@ package be.elmital.fixmcstats;
 
 import be.elmital.fixmcstats.utils.StatisticUtils;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.synchronization.ArgumentTypeInfos;
+import net.minecraft.commands.synchronization.SingletonArgumentInfo;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
@@ -34,19 +36,24 @@ public class FixMCStats {
             public void sendClientFeedBack(CommandSourceStack clientCommandSource, Component text) {
                 clientCommandSource.sendSystemMessage(text);
             }
-
         });
     }
 
-    @SubscribeEvent // on the mod event bus
+    @SubscribeEvent
     public static void register(RegisterEvent event) {
         event.register(BuiltInRegistries.CUSTOM_STAT.key(), registry -> {
             StatisticUtils.registerAllCustomStats(Constants.LOGGER);
         });
 
-        event.register(Registries.COMMAND_ARGUMENT_TYPE,registry -> {
+        event.register(Registries.COMMAND_ARGUMENT_TYPE, registry -> {
+            /*
+                Hacky way to register the serializer we can't use the BasicCommand.PATCH_ARGUMENT built one cause the method to register them is parameterized and don't allow this.
+                The method used by Fabric for the ArgumentType registration delegates to the vanilla method that is private and not accessible here.
+             */
             BasicCommand.notifyArgumentRegisteringStarting();
-            registry.register(BasicCommand.PATCH_ARGUMENT.id, BasicCommand.PATCH_ARGUMENT.serializer);
+            var serializer = ArgumentTypeInfos.registerByClass(BasicCommand.PatchArgumentType.class, SingletonArgumentInfo.contextAware(access -> BasicCommand.PatchArgumentType.patchArgument(null)));
+            ArgumentTypeInfos.registerByClass(BasicCommand.PatchActionArgumentType.class, BasicCommand.PATCH_ACTION_ARGUMENT.serializer);
+            registry.register(BasicCommand.PATCH_ARGUMENT.id, serializer);
             registry.register(BasicCommand.PATCH_ACTION_ARGUMENT.id, BasicCommand.PATCH_ACTION_ARGUMENT.serializer);
             BasicCommand.notifyArgumentRegisteringEnding();
         });
