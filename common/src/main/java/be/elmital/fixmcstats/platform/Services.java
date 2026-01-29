97bc4c7af8f10614f4687dbd3bf2906294f8fd7b
elmital <54907162+elmital@users.noreply.github.com>
2026-01-29 15:34:04 +0100

    fix: force the usage of a specific class loader when loading Services
    (cherry picked from commit e14dfa172e5595e4794911863a53dbcf5226f54c)

--- common/src/main/java/be/elmital/fixmcstats/platform/Services.java
+++ common/src/main/java/be/elmital/fixmcstats/platform/Services.java
@@ -5,25 +5,25 @@ import be.elmital.fixmcstats.platform.services.IPlatformHelper;
 
 import java.util.ServiceLoader;
 
-// Service loaders are a built-in Java feature that allow us to locate implementations of an interface that vary from one
-// environment to another. In the context of MultiLoader we use this feature to access a mock API in the common code that
-// is swapped out for the platform specific implementation at runtime.
+/*
+    Service loaders are a built-in Java feature that allow us to locate implementations of an interface that vary from
+    one environment to another. In this context we use this feature to access a mock API in the common code that
+    is swapped out for the platform specific implementation at runtime.
+ */
 public class Services {
 
-    // In this example we provide a platform helper which provides information about what platform the mod is running on.
-    // For example this can be used to check if the code is running on Forge vs Fabric, or to ask the modloader if another
-    // mod is loaded.
+     // We provide a platform helper which provides information about what platform the mod is running on.
     public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
 
-    // This code is used to load a service for the current environment. Your implementation of the service must be defined
-    // manually by including a text file in META-INF/services named with the fully qualified class name of the service.
-    // Inside the file you should write the fully qualified class name of the implementation to load for the platform. For
-    // example our file on Forge points to ForgePlatformHelper while Fabric points to FabricPlatformHelper.
+    /*
+        This code is used to load a service for the current environment. The implementation of the service is defined
+        manually by including a text file in META-INF/services named with the fully qualified class name of the service.
+        For example our file on NeoForge points to NeoForgePlatformHelper while Fabric points to FabricPlatformHelper.
+     */
     public static <T> T load(Class<T> clazz) {
-
-        final T loadedService = ServiceLoader.load(clazz)
+        final T loadedService = ServiceLoader.load(clazz, clazz.getClassLoader()) // Force this specific class loader to prevent potential conflicts with other mods and allows it to be cached safely @see ServiceLoader#load(Class) API notes
                 .findFirst()
-                .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
+                .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName() + " using the classloader " + clazz.getClassLoader().getName()));
         Constants.LOGGER.debug("Loaded {} for service {}", loadedService, clazz);
         return loadedService;
     }
