4f1f2d1ddaea0bea58b68fc5bc4847f6b098f2df
elmital <54907162+elmital@users.noreply.github.com>
2026-06-17 15:38:48 +0200

    fix: PotionSlotMixin use inject instead of overriding the method
    (cherry picked from commit 66e08575a05ea2f5de119f91b163fa051fee2f71)

--- common/src/main/java/be/elmital/fixmcstats/mixin/PotionSlotMixin.java
+++ common/src/main/java/be/elmital/fixmcstats/mixin/PotionSlotMixin.java
@@ -2,24 +2,20 @@ package be.elmital.fixmcstats.mixin;
 
 
 import be.elmital.fixmcstats.Configs;
-import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.BrewingStandMenu;
-import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BrewingStandMenu.PotionSlot.class)
-public class PotionSlotMixin extends Slot {
+public class PotionSlotMixin {
     // Fix https://bugs.mojang.com/browse/MC-154487
-    public PotionSlotMixin(Container inventory, int index, int x, int y) {
-        super(inventory, index, x, y);
-    }
-
-    @Override
-    public void onTake(Player player, ItemStack stack) {
-        super.onTake(player, stack);
+    @Inject(method = "onTake", at = @At("TAIL"))
+    public void onTake(Player player, ItemStack carried, CallbackInfo ci) {
         if (Configs.CRAFTING_POTION_FIX.isActive())
-            stack.onCraftedBy(player, stack.getCount());
+            carried.onCraftedBy(player, carried.getCount());
     }
 }
