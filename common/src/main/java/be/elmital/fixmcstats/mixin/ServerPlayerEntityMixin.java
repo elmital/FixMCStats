74aad7c45083bd6c9e179320c4e5b326c0eb116d
elmital <54907162+elmital@users.noreply.github.com>
2026-07-24 18:24:55 +0200

    fix(MC-256638 & MC-277294): ensure custom stats are used before modifying the Identifier

--- common/src/main/java/be/elmital/fixmcstats/mixin/ServerPlayerEntityMixin.java
+++ common/src/main/java/be/elmital/fixmcstats/mixin/ServerPlayerEntityMixin.java
@@ -58,7 +58,9 @@ public abstract class ServerPlayerEntityMixin extends Player {
     // Fix https://bugs.mojang.com/browse/MC-277294
     @ModifyArg(method = "checkRidingStatistics", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;awardStat(Lnet/minecraft/resources/Identifier;I)V", ordinal = 3), index = 0)
     private Identifier modifyTargetStat(Identifier identifier) {
-        if (getVehicle() instanceof Camel && Configs.CAMEL_STAT.isActive())
+        if (!Configs.USE_CUSTOM_STATS.isActive())
+            return identifier;
+        else if (getVehicle() instanceof Camel && Configs.CAMEL_STAT.isActive())
             return StatisticUtils.CAMEL_RIDING_STAT.identifier();
         else if (getVehicle() instanceof Donkey && Configs.USE_DONKEY_STATS.isActive())
             return StatisticUtils.DONKEY_RIDING_STAT.identifier();
