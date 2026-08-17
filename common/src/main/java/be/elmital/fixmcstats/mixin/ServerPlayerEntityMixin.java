93909782de402d669fd7163b31b2ab568e44c213
elmital <54907162+elmital@users.noreply.github.com>
2026-08-17 15:02:35 +0200

    Custom stats disabled issues (#54)
    * fix(MC-256638 & MC-277294): ensure custom stats are used before modifying the Identifier
    
    * fix(MC-148457): ensure the usage of Custom stats before awarding the stat and cancel the increment if its not the case
    
    (cherry picked from commit e3c84e48172690842ec0dfa2478cd1f68da4df5c)

--- common/src/main/java/be/elmital/fixmcstats/mixin/ServerPlayerEntityMixin.java
+++ common/src/main/java/be/elmital/fixmcstats/mixin/ServerPlayerEntityMixin.java
@@ -62,7 +62,9 @@ public abstract class ServerPlayerEntityMixin extends Player {
     // Fix https://bugs.mojang.com/browse/MC-277294
     @ModifyArg(method = "checkRidingStatistics", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;awardStat(Lnet/minecraft/resources/ResourceLocation;I)V", ordinal = 3), index = 0)
     private ResourceLocation modifyTargetStat(ResourceLocation identifier) {
-        if (getVehicle() instanceof Camel && Configs.CAMEL_STAT.isActive())
+        if (!Configs.USE_CUSTOM_STATS.isActive())
+            return identifier;
+        else if (getVehicle() instanceof Camel && Configs.CAMEL_STAT.isActive())
             return StatisticUtils.CAMEL_RIDING_STAT.identifier();
         else if (getVehicle() instanceof Donkey && Configs.USE_DONKEY_STATS.isActive())
             return StatisticUtils.DONKEY_RIDING_STAT.identifier();
@@ -105,10 +107,19 @@ public abstract class ServerPlayerEntityMixin extends Player {
         }
     }
 
+
+    // Fix for https://bugs.mojang.com/browse/MC-148457
+    @Inject(method = "checkMovementStatistics", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;awardStat(Lnet/minecraft/resources/ResourceLocation;I)V", ordinal = 6), cancellable = true)
+    private void cancelStatIncrement(double dx, double dy, double dz, CallbackInfo ci) {
+        if (this.isVisuallyCrawling() && Configs.CRAWL_STAT.isActive() && !Configs.USE_CUSTOM_STATS.isActive()) {
+            ci.cancel();
+        }
+    }
+
     // Fix for https://bugs.mojang.com/browse/MC-148457
     @ModifyArg(method = "checkMovementStatistics", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;awardStat(Lnet/minecraft/resources/ResourceLocation;I)V", ordinal = 6), index = 0)
     private ResourceLocation useCrawlStat(ResourceLocation identifier) {
-        if (Configs.CRAWL_STAT.isActive() && this.isVisuallyCrawling())
+        if (this.isVisuallyCrawling() && Configs.CRAWL_STAT.isActive() && Configs.USE_CUSTOM_STATS.isActive())
             return StatisticUtils.CRAWL_ONE_CM.identifier();
         return identifier;
     }
